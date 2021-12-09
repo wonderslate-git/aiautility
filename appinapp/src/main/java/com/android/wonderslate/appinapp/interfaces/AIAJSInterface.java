0258c93b523bca3816c3221ba8fd5168ce05271a
Anirudha <anirudha.wonderslate@gmail.com>
2021-12-09 11:10:13 +0530

    type: fix for Payment
    body:
    1. fixed payment issue
    2. increased library version

--- appinapp/src/main/java/com/android/wonderslate/appinapp/interfaces/AIAJSInterface.java
+++ appinapp/src/main/java/com/android/wonderslate/appinapp/interfaces/AIAJSInterface.java
@@ -8,71 +8,27 @@ import android.webkit.WebView;
 
 import com.android.wonderslate.appinapp.data.local.WSSharedPrefs;
 import com.android.wonderslate.appinapp.util.PaymentUtility;
+import com.android.wonderslate.appinapp.views.AIAActivity;
 import com.razorpay.Checkout;
 import com.razorpay.PaymentResultListener;
 
 import org.json.JSONObject;
 
-public class AIAJSInterface implements PaymentResultListener {
+public class AIAJSInterface {
     private Activity context;
     private WebView webView;
     WSSharedPrefs wsSharedPrefs;
+    AIAActivity aiaActivity;
 
     public AIAJSInterface(WebView webView, Activity context) {
         this.webView = webView;
         this.context = context;
         wsSharedPrefs = WSSharedPrefs.getInstance(context);
+        aiaActivity = new AIAActivity();
     }
 
     @JavascriptInterface
     public void startPayment(String bookId, String price, String bookTitle) {
-        Log.d("AppInApp", "start payment");
-        Log.d("AppInApp", "id " + bookId);
-        Log.d("AppInApp", "title " + bookTitle);
-        Log.d("AppInApp", "price " + price);
-
-        final Activity activity = context;
-        final Checkout co = new Checkout();
-        //co.setImage(R.mipmap.ic_launcher); //Company logo
-        try {
-            JSONObject options = new JSONObject();
-            options.put("name", "Dear Sir"); //Company Name
-            options.put("description", bookTitle + " (Course " + bookId + ")");
-            //You can omit the image option to fetch the image from dashboard
-            options.put("currency", "INR");
-            options.put("amount", (Double.parseDouble(price) * 100) + "");
-            JSONObject preFill = new JSONObject();
-            preFill.put("name", wsSharedPrefs.getUserName());
-            preFill.put("email", "anirudha.wonderslate@gmail.com"); //hardcoded email Todo: get the user email from the client app
-            preFill.put("contact", wsSharedPrefs.getUsermobile());
-            JSONObject ReadOnly = new JSONObject();
-            ReadOnly.put("email", "true");
-            ReadOnly.put("contact", "true");
-            JSONObject theme = new JSONObject();
-            theme.put("color", "#F05A2A");
-            options.put("prefill", preFill);
-            options.put("theme", theme);
-            options.put("readonly", ReadOnly);
-            JSONObject notes = new JSONObject();
-            notes.put("bookId", bookId);
-            notes.put("username", wsSharedPrefs.getUsermobile());
-            options.put("notes", notes);
-
-            co.open(activity, options);
-        } catch (Exception e) {
-//            Utils.showErrorToast(ShopBookDetailsActivity.this, -3);
-            Log.e("AppInApp", "Payment Failure", e);
-        }
-    }
-
-    @Override
-    public void onPaymentSuccess(String s) {
-        Log.d("AppInApp", "Razorpay Payment Success");
-        Log.d("AppInApp", "Razorpay Payment Details: " + s);
-    }
-
-    @Override
-    public void onPaymentError(int i, String s) {
-        Log.e("AppInApp", "Payment Failure: " + s + " code: " + i);
+        aiaActivity.startPayment(bookId, bookTitle, price, webView);
     }
 }
---
+++ appinapp/src/main/java/com/android/wonderslate/appinapp/views/AIAActivity.java
@@ -0,0 +1,85 @@
+package com.android.wonderslate.appinapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.android.wonderslate.appinapp.data.local.WSSharedPrefs;
+import com.google.android.material.snackbar.Snackbar;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class AIAActivity extends AppCompatActivity implements PaymentResultListener {
+    WSSharedPrefs wsSharedPrefs;
+    WebView webView;
+    String bookId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        wsSharedPrefs = WSSharedPrefs.getInstance(this);
+    }
+
+    public void startPayment(String bookId, String bookTitle, String price, WebView webView) {
+        Log.d("AppInApp", "start payment");
+        Log.d("AppInApp", "id " + bookId);
+        Log.d("AppInApp", "title " + bookTitle);
+        Log.d("AppInApp", "price " + price);
+
+        this.webView = webView;
+        this.bookId = bookId;
+
+        final Activity activity = this;
+        final Checkout co = new Checkout();
+        //co.setImage(R.mipmap.ic_launcher); //Company logo
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Dear Sir"); //Company Name
+            options.put("description", bookTitle + " (Course " + bookId + ")");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("currency", "INR");
+            options.put("amount", (Double.parseDouble(price) * 100) + "");
+            JSONObject preFill = new JSONObject();
+            preFill.put("name", wsSharedPrefs.getUserName());
+            preFill.put("email", "anirudha.wonderslate@gmail.com"); //hardcoded email Todo: get the user email from the client app
+            preFill.put("contact", wsSharedPrefs.getUsermobile());
+            JSONObject ReadOnly = new JSONObject();
+            ReadOnly.put("email", "true");
+            ReadOnly.put("contact", "true");
+            JSONObject theme = new JSONObject();
+            theme.put("color", "#F05A2A");
+            options.put("prefill", preFill);
+            options.put("theme", theme);
+            options.put("readonly", ReadOnly);
+            JSONObject notes = new JSONObject();
+            notes.put("bookId", bookId);
+            notes.put("username", wsSharedPrefs.getUsermobile());
+            options.put("notes", notes);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+//            Utils.showErrorToast(ShopBookDetailsActivity.this, -3);
+            Log.e("AppInApp", "Payment Failure", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Log.d("AppInApp", "Razorpay Payment Success");
+        Log.d("AppInApp", "Razorpay Payment Details: " + s);
+
+        webView.evaluateJavascript("buyBookAppinApp("+s+","+bookId+","+""+","+""+");", null);
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Log.e("AppInApp", "Payment Failure: " + s + " code: " + i);
+        Toast.makeText(this, "Payment Failure: " + s, Toast.LENGTH_SHORT).show();
+    }
+}
