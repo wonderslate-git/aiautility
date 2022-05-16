25bb12c94eb03a3489254f54614df35988f6b472
Anirudha <anirudha.wonderslate@gmail.com>
2022-05-16 16:34:44 +0530

    Type: Config
    Body:
    1. Upgraded library version

--- appinapp/src/main/java/com/android/wonderslate/appinapp/data/remote/ServerURLManager.java
+++ appinapp/src/main/java/com/android/wonderslate/appinapp/data/remote/ServerURLManager.java
@@ -31,7 +31,7 @@ public final class ServerURLManager {
 
     public void setService() {
         //Select Base Service URL According To Server.
-        currentServer = Servers.QADEV; //Use QA, STAGING, PUBLISH, LIVE
+        currentServer = Servers.QA; //Use QA, STAGING, PUBLISH, LIVE
 
         switch (currentServer){
             case QA:
