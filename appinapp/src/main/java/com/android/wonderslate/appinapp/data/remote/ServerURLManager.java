543ec1499c229be4494ba9b9f5e14c7260fd48c2
Anirudha <anirudha.wonderslate@gmail.com>
2022-05-16 12:58:13 +0530

    Type: Config
    Body:
    1. Library connected to Staging
    2. Upgraded library version

--- appinapp/src/main/java/com/android/wonderslate/appinapp/data/remote/ServerURLManager.java
+++ appinapp/src/main/java/com/android/wonderslate/appinapp/data/remote/ServerURLManager.java
@@ -30,7 +30,7 @@ public final class ServerURLManager {
 
     public void setService() {
         //Select Base Service URL According To Server.
-        currentServer = Servers.QA; //Use QA, STAGING, PUBLISH, LIVE
+        currentServer = Servers.STAGING; //Use QA, STAGING, PUBLISH, LIVE
 
         switch (currentServer){
             case QA:
